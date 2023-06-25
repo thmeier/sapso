d0513614d5f2ee5d23aa94481bf89fb226f3672f
thmeier <thierry.s.meier@gmail.com>
2023-06-25 15:25:49 +0200

    extend unit_test

--- unit_test.py
+++ unit_test.py
@@ -4,10 +4,15 @@ These computations are non-sensical and only relevant for testing purposes.
 """
 import sapso
 
-tfunc = sapso.test.Eggholder()
-
 kwargs_sa = { 'iterations' : 100, 'seed' : 42, 'temperature' : None, 'step_size' : 0.1, 'goal' : 'min' }
 kwargs_pso = { 'iterations' : 100, 'seed' : 42, 'n_particles' : 10, 'w' : 0.75, 'a_ind' : 1., 'a_neigh' : 2., 'goal' : 'min' }
+
+tfunc = sapso.test.Eggholder()
+
+SA = sapso.sa.SimulatedAnnealing(tfunc.objective, tfunc.area, **kwargs_sa)
+PSO = sapso.pso.ParticleSwarmOptimization(tfunc.objective, tfunc.area, **kwargs_pso)
+
+tfunc = sapso.test.SchaffnerNo4()
 
 SA = sapso.sa.SimulatedAnnealing(tfunc.objective, tfunc.area, **kwargs_sa)
 PSO = sapso.pso.ParticleSwarmOptimization(tfunc.objective, tfunc.area, **kwargs_pso)
@@ -16,4 +21,5 @@ history_sa = SA.optimize()
 history_pso = PSO.optimize()
 
 sapso.utils.print_results(history_sa, tfunc, end='\n\n')
-sapso.utils.print_results(history_sa, sapso.test.Himmelblau())
+sapso.utils.print_results(history_pso, tfunc)
+
